f982384c19f552eaff8f6d243b5a9c5c396de927
Martin Grund <grundprinzip@gmail.com>
2010-11-29 08:40:43 +0100

    gnuplot

--- Library/Formula/gnuplot.rb
+++ Library/Formula/gnuplot.rb
@@ -2,23 +2,20 @@ require 'formula'
 
 class Gnuplot <Formula
   url 'http://downloads.sourceforge.net/project/gnuplot/gnuplot/4.4.0/gnuplot-4.4.0.tar.gz'
-  homepage 'http://www.gnuplot.info'
+  homepage 'www.gnuplot.info'
   md5 'e708665bd512153ad5c35252fe499059'
 
-  depends_on 'pkg-config'
   depends_on 'readline'
+  depends_on 'aquaterm'
   depends_on 'gd' unless ARGV.include? "--nogd"
   depends_on 'pdflib-lite' if ARGV.include? "--pdf"
 
   def install
     ENV.x11
-    args = ["--disable-debug", "--disable-dependency-tracking",
-            "--prefix=#{prefix}",
-            "--with-readline=#{prefix}",
-            "--disable-wxwidgets"]
-    args << "--without-lua" if ARGV.include? "--without-lua"
-
-    system "./configure", *args
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-readline=#{prefix}",
+                          "--disable-wxwidgets"
     system "make install"
   end
 end
